e486356865d289d2ff043a764d5613b0b21e519f
sviridov-andrey <chosenfew@yandex.ru>
2023-12-20 17:49:58 +0300

    _

--- education/models.py
+++ education/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from users.models import User
 from users.utils import NULLABLE
 
 
@@ -29,3 +30,17 @@ class Leson(models.Model):
     class Meta:
         verbose_name = 'Урок'
         verbose_name_plural = 'Уроки'
+
+
+class Payment(models.Model):
+    CHOICES_PAYMENT_METHOD = (
+        ('transfer', 'онлайн'),
+        ('cash', 'наличные')
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, **NULLABLE)
+    payment_date = models.DateTimeField(verbose_name='дата оплаты')
+    paid_course = models.ForeignKey(Course, on_delete=models.CASCADE, **NULLABLE)
+    paid_leson = models.ForeignKey(Leson, on_delete=models.CASCADE, **NULLABLE)
+    payment_method = (models.CharField(max_length=15, choices=CHOICES_PAYMENT_METHOD,
+                                       verbose_name='способ оплаты', default='transfer'))
--- education/serializers.py
+++ education/serializers.py
@@ -4,10 +4,14 @@ from education.models import Course, Leson
 
 
 class CourseSerializers(serializers.ModelSerializer):
+    lesons_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+        def get_lesons_count(self, instance):
+            return instance.leson_set.count()
 
 
 class LesonSerializers(serializers.ModelSerializer):
