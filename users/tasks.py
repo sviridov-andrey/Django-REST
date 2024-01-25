4040cd654a57c5f7b2d4529f113157274b7236e2
Андрей <chosenfew@yandex.ru>
2024-01-25 21:23:22 +0300

    Написан Dockerfile

--- users/tasks.py
+++ users/tasks.py
@@ -1,21 +1,21 @@
 from celery import shared_task
 import datetime
+
+from dateutil.relativedelta import relativedelta
+
 from users.models import User
 
 
 @shared_task
 def check_user_activity():
     """Проверка непосещения сайта пользователем более 30 дней"""
-    users = User.objects.filter(is_active=True)
+
     current_date = datetime.datetime.now()
+    month_ago = current_date - relativedelta(months=1)
+    users = User.objects.filter(is_active=True, last_login__lte=month_ago)
 
     if users:
         for user in users:
-            if user.last_login:
-                user_last_login = user.last_login
-                if (current_date - user_last_login.date()).days > 30:
-                    user.is_active = False
-            else:
-                user.last_login = current_date
+            user.is_active = False
 
-        User.objects.bulk_update(users, ['is_active', 'last_login'])
+    User.objects.bulk_update(users, ['is_active'])
